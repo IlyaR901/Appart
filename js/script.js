75be3cebddcd963bb622dd452929503b8fcc2973
IlyaR901 <103831515+IlyaR901@users.noreply.github.com>
2022-04-21 21:59:47 +0300

    Update burger_menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-    $('.item_video svg').mouseover(function() {
-        $(".item_video svg").fadeTo(100 , 0.3);
+    $('.item_video svg').mouseover(function () {
+        $(".item_video svg").fadeTo(100, 0.3);
     });
-    $('.item_video svg').click(function() {
+    $('.item_video svg').click(function () {
         $('.item_video svg').css('display', 'none');
-    });   
-    
-    
+    });
+    $('.burger').click(function () {
+        $('.burger, .nav').toggleClass('active');
+    });
+
 
 });
