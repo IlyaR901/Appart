2065de3d99c97e0ee0863f286d3f558bb77f1f20
IlyaR901 <103831515+IlyaR901@users.noreply.github.com>
2022-04-21 14:37:53 +0300

    First project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+
+    $('.item_video svg').mouseover(function() {
+        $(".item_video svg").fadeTo(100 , 0.3);
+    });
+    $('.item_video svg').click(function() {
+        $('.item_video svg').css('display', 'none');
+    });   
+    
+    
+
+});
